966fecc40a74baf2e5129481ddd982f8eaf8ac1e
unknown <rubyhankuk@gmail.com>
2024-03-01 21:32:35 +0900

    bu tajriba uchun

---
+++ index.js
@@ -0,0 +1,58 @@
+// *********Vazifa1*********
+// function findTriangleType(a,b,c) {
+//     if (a==b && b==c){
+//         console.log("Equilateral triangle")
+//     }
+//     else if(a==b||b==c|| a==c){
+//         console.log("Isosceles triangle")
+//     }
+//     else{
+//         console.log("Scalene triangle")
+//     }
+
+// }
+// findTriangleType(5,7,5)
+
+
+// *********Vazifa2*********
+
+// function checkLeapYear(year){
+//     if(year%4==0 && year%100!=0 || year%400==0){
+//         console.log(year, " is a leap year")
+//     }
+//     else{
+//         console.log(year, "is not a leap year")
+//     }
+// }
+// checkLeapYear(2100)
+
+
+// *********Vazifa3*********
+
+// function findGrade(grade){
+//     if(grade>=90 && grade<=100){
+//         console.log("A+ grade")
+//     }
+//     else if(grade>=80 && grade<=90){
+//         console.log("A grade")
+//     }
+//     else if(grade>=70 && grade<=80){
+//         console.log("B grade")
+//     }
+//     else if(grade>=60 && grade<=70){
+//         console.log("C grade")
+//     }
+//     else if(grade>=50 && grade<=60){
+//         console.log("D grade")
+//     }
+//     else if(grade>=40 && grade<=50){
+//         console.log("E grade")
+//     }
+//     else if(grade>=0 && grade<=40){
+//         console.log("Student has failed")
+//     }
+//     else{
+//         console.log("Invalid marks")
+//     }
+// }
+// findGrade(68)
