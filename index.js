1d0c509f55b5ec6185d98b05adfdb2841b730a1c
unknown <rubyhankuk@gmail.com>
2024-03-15 13:58:57 +0900

    Problems with solutions to learn coding with JavaScript

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // *********Vazifa1*********
+// Function findTriangleType() finds the type of the triangle for given side values by using “==” and “&&” operator in JavaScript.
+//  1 Print “Equilateral triangle.” if values for all side1, side2 and side3 are equal.
+//  2 Print “Isosceles triangle.” if values for side1 is equal to side2 or side2 is equal to side3
+//  3 Else “Scalene triangle.” since values of all sides are unequal.
+
+
 // function findTriangleType(a,b,c) {
 //     if (a==b && b==c){
 //         console.log("Equilateral triangle")
@@ -15,6 +21,12 @@
 
 
 // *********Vazifa2*********
+// Function checkLeapYear() find if the given year is a leap year or not by using %, !=, && and || operators in JavaScript.
+
+//  1 If year is divisble by 4 and not divisble by 100 then print “leap year”.
+//  2 Or if year is divisible by 400 then print “leap year”.
+//  3 Else print “not a leap year”.
+
 
 // function checkLeapYear(year){
 //     if(year%4==0 && year%100!=0 || year%400==0){
@@ -28,6 +40,16 @@
 
 
 // *********Vazifa3*********
+// Function findGrade() to find the grade of the student based on the input marks.
+//  1 Print “A+ grade” if marks is between 90 and 100.
+//  2 Print “A grade” if marks is between 80 and 90.
+//  3 Print “B grade” if marks is between 70 and 80.
+//  4 Print “C grade” if marks is between 60 and 70.
+//  5 Print “D grade” if marks is between 50 and 60.
+//  6 Print “E grade” if marks is between 40 and 50.
+//  7 Print “Student has failed” if marks is between 0 and 40.
+//  8 Else print “Invalid marks”.
+
 
 // function findGrade(grade){
 //     if(grade>=90 && grade<=100){
